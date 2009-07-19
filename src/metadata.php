64a5108452e426b67e3bb7c2afa6cbe815a15544
Martii <thalamew@hotmail.com>
2009-07-19 03:37:55 +0000

    Added contributor
    git-svn-id: https://usocheckup.googlecode.com/svn/trunk@6 c11ff0bc-73fa-11de-87a4-b5b95bed8975

--- src/metadata.php
+++ src/metadata.php
@@ -5,7 +5,7 @@
       "xmlns":        "usoCheckup:",
       "copyright":    "Copyright (C) 2009+, http://userscripts.org",
       "author":       "Marti Martz (http://userscripts.org/users/37004)",
-      "version":      "5",
+      "version":      "6",
       "license":      "http://usocheckup.googlecode.com/svn/trunk/license.txt",
       "changelog":    "http://code.google.com/p/usocheckup/source/list",
       "contributor": [
@@ -16,7 +16,8 @@
         "Johan Sundström (http://userscripts.org/users/326)",
         "lucideer (http://userscripts.org/users/56750)",
         "Photodeus (http://userscripts.org/users/16828)",
-        "sizzlemctwizzle (http://userscripts.org/users/27715)"
+        "sizzlemctwizzle (http://userscripts.org/users/27715)",
+        "Tim Smart (http://userscripts.org/users/63868)"
       ]
     }';
 
